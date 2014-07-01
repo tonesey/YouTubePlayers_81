c00cfee7e5f648ad9a4544462c4e9e45450d194d
davide.tentori@hotmail.com <davide.tentori@hotmail.com>
2014-07-01 22:53:41 +0200

    added testers

---
+++ WP81Test/PanoramaApp1/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.BackgroundAudio;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace PanoramaApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+        // http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh758325.aspx
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // var files = (await KnownFolders.MusicLibrary.GetFilesAsync()).ToList();
+            // var files1 = (await KnownFolders.RemovableDevices.GetFilesAsync()).ToList();
+
+            StorageFolder externalDevices = Windows.Storage.KnownFolders.RemovableDevices;
+            // Get the first child folder, which represents the SD card.
+            StorageFolder sdCard = (await externalDevices.GetFoldersAsync()).FirstOrDefault();
+
+            if (sdCard != null)
+            {
+                //An SD card is present and the sdCard variable now contains a reference to it.
+
+                var sdFiles = (await sdCard.GetFilesAsync()).ToList();
+
+                //StorageFolder folder = (await externalDevices.GetFoldersAsync()).FirstOrDefault();
+
+                
+                //file write
+                StorageFile sampleFile = await sdCard.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+                IRandomAccessStream stream = await sampleFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+                using (var outputStream = stream.GetOutputStreamAt(0))
+                {
+                    DataWriter dataWriter = new DataWriter(outputStream);
+                    dataWriter.WriteString("sd file sample content");
+                    await dataWriter.StoreAsync();
+                    await outputStream.FlushAsync();
+                }
+
+
+                //using (IsolatedStorageFileStream stream = new IsolatedStorageFileStream(curEpisodeFileName, System.IO.FileMode.Create, _isoStore))
+                //{
+                //    byte[] buffer = new byte[1024];
+                //    //while (e.Result.Read(buffer, 0, buffer.Length) > 0)
+                //    //{
+                //    //    stream.Write(buffer, 0, buffer.Length);
+                //    //}
+                //    //18-07-2013 - così non viene scritto tutto il buffer ma solo i dati realmente necessari
+                //    int bytesRead;
+                //    while ((bytesRead = e.Result.Read(buffer, 0, buffer.Length)) > 0)
+                //    {
+                //        stream.Write(buffer, 0, bytesRead);
+                //    }
+                //}
+
+                //file read
+                //var stream = await sampleFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                StorageFile readFile = await sdCard.GetFileAsync("sample.txt");
+                string text = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+                MessageBox.Show(text);
+
+            }
+            else
+            {
+                // No SD card is present.
+                MessageBox.Show("SD card not present");
+            }
+        }
+
+        async void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            var stream = e.Result;
+            //http://stackoverflow.com/questions/7669311/is-there-a-way-to-convert-a-system-io-stream-to-a-windows-storage-streams-irando
+
+            //Windows.Storage.Streams.IInputStream inStream = stream.AsInputStream();
+            //Windows.Storage.Streams.IOutputStream outStream = stream.AsOutputStream();
+
+
+
+
+            //using (var outputStream = outStream)
+            //{
+            //    DataWriter dataWriter = new DataWriter(outputStream);
+            //    dataWriter.WriteString("sd file sample content");
+            //    await dataWriter.StoreAsync();
+            //    await outputStream.FlushAsync();
+            //}
+
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+            client.OpenReadCompleted += client_OpenReadCompleted;
+            client.DownloadProgressChanged += client_DownloadProgressChanged;
+            client.OpenReadAsync(new Uri("http://TODO"));
+
+
+            var imageUrl = "http://www.microsoft.com/global/en-us/news/publishingimages/logos/MSFT_logo_Web.jpg";
+            var client = new HttpClient();
+            Stream stream = await client.GetStreamAsync(imageUrl);
+            var memStream = new MemoryStream();
+            await stream.CopyToAsync(memStream);
+
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ WP81Test/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Resources;
+using Windows.Storage;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+
+
+            TestSD();
+          
+
+
+
+        }
+
+        private async void TestSD()
+        {
+            http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh758325.aspx
+
+            //StorageFolder test1 = Windows.Storage.KnownFolders.MusicLibrary;
+
+            // Get the logical root folder for all external storage devices.
+            StorageFolder externalDevices = Windows.Storage.KnownFolders.RemovableDevices;
+
+            // Get the first child folder, which represents the SD card.
+            StorageFolder sdCard = (await externalDevices.GetFoldersAsync()).FirstOrDefault();
+
+            if (sdCard != null)
+            {
+                // An SD card is present and the sdCard variable now contains a reference to it.
+            }
+            else
+            {
+                // No SD card is present.
+            }
+
+
+            //StorageFolder folder = KnownFolders.RemovableDevices;
+            //StorageFile sampleFile = await folder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
